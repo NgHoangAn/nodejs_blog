1704cb1b7077212e1bde62afbd76f5573b735306
Ng Hoang An <hoangan.cv0@gmail.com>
2022-11-17 19:11:17 +0700

    create routes, app/controller folder

--- src/index.js
+++ src/index.js
@@ -2,22 +2,30 @@ const express = require('express')
 const morgan = require('morgan')
 const hbs = require('express-handlebars')
 const path = require('path');
-const app = express()
+const app = express();
 const port = 4000
+
+const route = require('./routes');
 //static 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//middleware
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
 
 //http logger
 app.use(morgan('combined'));
 //template engine
 app.engine('hbs', hbs.engine({
-    extname:'.hbs'
+    extname: '.hbs'
 }));
 
-app.set('view engine','hbs')
-app.set('views', path.join(__dirname,'resources','views'))
-app.get('/', (req,res) => {
-    res.render('home')
-})
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resources', 'views'));
+
+route(app);
+
 
 app.listen(port, () => console.log(`app listen at http://localhost:${port}`))
